856cfd9a49483fd90fe22813277bc71af807ed5d
Iván Pretel <gonzalo2004.2017@gmail.com>
2026-05-09 15:47:21 -0300

    Refactor route definitions to include API prefix and organize middleware imports

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,6 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import healthRoutes from './routes/health.routes.js';
 import authRoutes from './routes/auth.routes.js';
+import facultyRoutes from './routes/faculty.routes.js';
+import postRoutes from './routes/post.routes.js';
+import errorMiddleware from './middlewares/error.middleware.js';
 
 const app = express();
 
@@ -11,6 +14,11 @@ app.use(express.json());
 
 // Routes
 app.use('/', healthRoutes);
-app.use('/auth', authRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/faculties', facultyRoutes);
+app.use('/api/posts', postRoutes);
+
+// Error Middleware (DEBE estar al final)
+app.use(errorMiddleware);
 
 export default app;
