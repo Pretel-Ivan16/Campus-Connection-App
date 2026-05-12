2796f7a2c4d3ab26f752069a6627f23c9c74b501
Iván Pretel <gonzalo2004.2017@gmail.com>
2026-05-12 14:12:55 -0300

    Refactor Layout component to include Header and Footer with responsive structure

--- Frontend/src/components/layout/Layout.tsx
+++ Frontend/src/components/layout/Layout.tsx
@@ -1,9 +1,15 @@
+import { Outlet } from 'react-router-dom'
 import Header from "./header/Header"
+import Footer from "./footer/Footer"
 
 function Layout() {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Header />
+      <main className="grow">
+        <Outlet />
+      </main>
+      <Footer />
     </div>
   )
 }
