023b973ea88bb011b04123271a9ecca5e6449bcb
Iván Pretel <gonzalo2004.2017@gmail.com>
2026-05-12 14:12:06 -0300

    Refactor BrandLogo component to use Link from react-router-dom for navigation

--- Frontend/src/components/layout/header/BrandLogo.tsx
+++ Frontend/src/components/layout/header/BrandLogo.tsx
@@ -1,12 +1,13 @@
+import { Link } from "react-router-dom"
 import { Logo } from "../../Logo"
 import TittleLogo from "../../TittleLogo"
 
 function BrandLogo() {
   return (
-    <a href="/" className="flex items-center gap-1 sm:gap-2 lg:gap-3">
+    <Link to="/" className="flex items-center gap-1 sm:gap-2 lg:gap-3">
       <Logo />
       <TittleLogo />
-    </a>
+    </Link>
   )
 }
 
