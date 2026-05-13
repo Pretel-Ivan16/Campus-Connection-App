fe7667c5b1bf093a033270b28be0cb6400cbd573
Iván Pretel <gonzalo2004.2017@gmail.com>
2026-05-12 22:19:31 -0300

    Add ProtectedRoute component for route protection and implement routing in AppRoutes

---
+++ Frontend/src/routes/index.tsx
@@ -0,0 +1,38 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from '../context/AuthContext';
+import { ProtectedRoute } from './ProtectedRoute';
+import { ErrorBoundary } from '../components/ErrorBoundary';
+import Home from '../pages/Home';
+import Login from '../pages/Login/Login';
+import Layout from '../components/layout/Layout';
+
+export default function AppRoutes() {
+  return (
+    <ErrorBoundary>
+      <BrowserRouter>
+        <AuthProvider>
+          <Routes>
+            {/* Rutas públicas */}
+            <Route path="/login" element={<Login />} />
+
+            {/* Rutas protegidas con layout */}
+            <Route
+              element={
+                <ProtectedRoute>
+                  <Layout />
+                </ProtectedRoute>
+              }
+            >
+              <Route path="/home" element={<Home />} />
+              <Route path="/" element={<Navigate to="/home" replace />} />
+              {/* Aquí irán más rutas protegidas */}
+            </Route>
+
+            {/* 404 */}
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </AuthProvider>
+      </BrowserRouter>
+    </ErrorBoundary>
+  );
+}
