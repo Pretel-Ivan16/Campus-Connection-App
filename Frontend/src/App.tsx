c9fb8866214d51a3235955f72789513a282b254d
Iván Pretel <gonzalo2004.2017@gmail.com>
2026-05-12 22:20:15 -0300

    Refactor App component to use AppRoutes for routing

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,22 +1,7 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Layout from './components/layout/Layout'
-import Home from './pages/Home'
+import AppRoutes from './routes'
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          {/* Las siguientes rutas se agregarán después */}
-          {/* <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/profile/:id" element={<Profile />} />
-          <Route path="/posts" element={<Posts />} /> */}
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  )
+  return <AppRoutes />
 }
 
 export default App
